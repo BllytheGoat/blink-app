3ca878f69da08af1b51ae2e4a72b39d4e33cb9e8
Billy Milles <okechukwujoshua2009@gmail.com>
2026-08-18 17:37:28 +0100

    Initial commit: sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'blink-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache).catch(() => {
+        // Offline cache may fail, but that's okay for PWA
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        // Offline fallback
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
